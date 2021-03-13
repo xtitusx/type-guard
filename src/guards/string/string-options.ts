e2931023cdab8085b521bfcba7ce7be758f0cac7
xtitusx <benjamin.tussac@gmail.com>
2021-03-13 14:49:07 +0100

    Fix IIsDecimalOptions TSdoc

--- src/guards/string/string-options.ts
+++ src/guards/string/string-options.ts
@@ -1,11 +1,11 @@
 export interface IIsDecimalOptions {
     /**
-     * Force string number to have a decimal separator.
+     * Force number string to have a decimal separator.
      * @defaultValue false
      */
     force?: boolean;
     /**
-     * Max number of digits to the right of the decimal separator in the string number.
+     * Max number of digits to the right of the decimal separator in the number string.
      */
     precision?: number;
 }
