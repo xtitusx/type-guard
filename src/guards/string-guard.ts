108bb434323e34ff7a7f74aecdfe74c7504a294a
xtitusx <benjamin.tussac@gmail.com>
2021-03-15 15:25:44 +0100

    Refactor TSdoc

--- src/guards/string-guard.ts
+++ src/guards/string-guard.ts
@@ -183,16 +183,23 @@ export class StringGuard extends Guard<StringRule> {
      * true:
      * - in 'firstChar' style, the first encountered character is mandatorily a letter.
      * - in 'startCase' style, the first character of each encountered word is mandatorily a letter.
+     *
      * false:
-     * - in 'firstChar' style, the first encountered character can be alphanumeric or special (even whitespace).
-     * - in 'startCase' style, the first character of each encountered word can be can be alphanumeric or special.
+     * - in 'firstChar' style, the first encountered character can be either alphanumeric or special (even whitespace).
+     * - in 'startCase' style, the first character of each encountered word can be either alphanumeric or special.
      * ```
      * @returns
+     * ```ts
      * - true with ('firstChar', true) params for values: "Foo", "F", "Foo#", "", "Thirty eight is my age"
-     * - false with ('firstChar', true) params for value: " Foo"
+     * - false with ('firstChar', true) params for value: " Foo", "#foo"
      * - true with ('firstChar', false) params for values: "Foo", "F", "#foo", "     foo", "", "38 is my age"
+     * - false with ('firstChar', false) params for values: "foo", "f", "#Foo", "     Foo", "38 is my Age"
+     *
      * - true with ('startCase', true) params for values: "The Quick Brown Fox Jumps Over The Lazy Dog.", " Foo, Bar."
-     * - true with ('startCase', false) params for value: "The Quick Brown Fox Jumps Over 1 Lazy Dog."
+     * - false with ('startCase', true) params for value: "1 Quick Brown Fox Jumps Over The Lazy Dog."
+     * - true with ('startCase', false) params for value: "1 Quick Brown Fox Jumps Over The Lazy Dog."
+     * - false with ('startCase', false) params for value: "1 quick Brown Fox Jumps Over The Lazy Dog."
+     * ```
      */
     public isCapitalized(style: CapitalizationStyle, checkFirstCharIsLetter?: boolean): this {
         this.addRule({ type: 'isCapitalized', style, checkFirstCharIsLetter });
